0b2d9e90854f88dc4b675a8034ca932d00767266
jackletter <1286317554@qq.com>
2017-02-17 16:41:41 +0800

    Update Program.cs

--- Manager/Program.cs
+++ Manager/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+/*********************************************
+ * 功能描述:自动定时服务管理端
+ * 创 建 人:胡庆杰
+ * 日    期:2017-2-8
+ * github:https://github.com/jackletter/JackTime2Run
+ * 说明:提供可视化管理支持
+ * 
+ ********************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
